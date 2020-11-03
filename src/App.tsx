3242ff04fb7b04ea0f28cf0b840d541cad8663bc
kaneru <kaneru@fastmail.com>
2020-11-03 16:59:59 +0900

    Add ESlint & Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import cn from 'classnames';
 import s from './App.module.scss';
 
 const App: React.FC = () => {
-  return (
-    <div className={cn(s.header, 'color')}>
-      Hello, world! This is an App component.
-    </div>
-  );
+  return <div className={cn(s.header, 'color')}>Hello, world! This is an App component.</div>;
 };
 
 export default App;
