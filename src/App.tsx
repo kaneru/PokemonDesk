00cc917b78ae47ecce0e14874a6bcd7cc19efb6b
kaneru <kaneru@fastmail.com>
2020-11-09 01:27:44 +0900

    feat: add Router to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/Home';
+import PokedexPage from './pages/Pokedex';
 
 const App: React.FC = () => {
-  return <HomePage />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/pokedex">
+          <PokedexPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
