2ef0328f77704d61c5fb38ce9c47cbca23a37711
kaneru <kaneru@fastmail.com>
2020-11-08 19:32:58 +0900

    feat: add HomePage to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import s from './App.module.scss';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import HomePage from './pages/Home';
 
 const App: React.FC = () => {
-  return (
-    <div className={s.site}>
-      <Header />
-      <div className={s.content}>Hello, world! This is an App component.</div>
-      <Footer />
-    </div>
-  );
+  return <HomePage />;
 };
 
 export default App;
