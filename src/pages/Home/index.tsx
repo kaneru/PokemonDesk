8f54048dac6738e406dfc9fd6bfe6a7fd29e00b4
kaneru <kaneru@fastmail.com>
2020-11-08 23:47:25 +0900

    feat: add Layout, Button and Parallax to Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+
+import Button from '../../components/Button';
 import Header from '../../components/Header';
+import Layout from '../../components/Layout';
+import Parallax from '../../components/Parallax';
 
 import s from './Home.module.scss';
 
@@ -7,6 +11,16 @@ const HomePage = () => {
   return (
     <div className={s.root}>
       <Header />
+      <Layout>
+        <div>
+          <h1>
+            <b>Find</b> all your favourite <b>Pokémon</b>
+          </h1>
+          <p>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
+          <Button onClick={() => {}}>See Pokémons</Button>
+        </div>
+        <Parallax />
+      </Layout>
     </div>
   );
 };
