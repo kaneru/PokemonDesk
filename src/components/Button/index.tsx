adef238c1f4b02f69eb174d741cd99f05baaeb7b
kaneru <kaneru@fastmail.com>
2020-11-09 00:11:02 +0900

    feat: add size prop to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,12 +6,13 @@ import s from './Button.module.scss';
 interface ButtonProps {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
   color?: 'yellow' | 'green';
+  size?: 'small' | 'medium' | 'large';
   isFullWidth?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick, isFullWidth, color = 'green' }) => {
+const Button: React.FC<ButtonProps> = ({ children, onClick, isFullWidth, color = 'green', size = 'medium' }) => {
   return (
-    <button type="button" className={cn(s.root, isFullWidth && s.fullWidth, s[color])} onClick={onClick}>
+    <button type="button" className={cn(s.root, isFullWidth && s.fullWidth, s[color], s[size])} onClick={onClick}>
       {children}
     </button>
   );
