0db5285f072430b489d1daf1e93548041014581f
kaneru <kaneru@fastmail.com>
2020-11-09 01:26:26 +0900

    feat: add Heading to Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Button from '../../components/Button';
 import Header from '../../components/Header';
+import Heading from '../../components/Heading';
 import Layout from '../../components/Layout';
 import Parallax from '../../components/Parallax';
 
@@ -12,11 +13,11 @@ const HomePage = () => {
     <div className={s.root}>
       <Header />
       <Layout>
-        <div>
-          <h1>
+        <div className={s.content}>
+          <Heading tag="h1">
             <b>Find</b> all your favourite <b>Pokémon</b>
-          </h1>
-          <p>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
+          </Heading>
+          <Heading tag="h4">You can know the type of Pokemon, its strengths, disadvantages and abilities</Heading>
           <Button onClick={() => {}}>See Pokémons</Button>
         </div>
         <Parallax />
