e956d039dd776ab9276d673c2619d5b29253d005
kaneru <kaneru@fastmail.com>
2020-11-09 00:06:31 +0900

    feat: add color prop to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,12 +5,13 @@ import s from './Button.module.scss';
 
 interface ButtonProps {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  color?: 'yellow' | 'green';
   isFullWidth?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick, isFullWidth }) => {
+const Button: React.FC<ButtonProps> = ({ children, onClick, isFullWidth, color = 'green' }) => {
   return (
-    <button type="button" className={cn(s.root, isFullWidth && s.fullWidth)} onClick={onClick}>
+    <button type="button" className={cn(s.root, isFullWidth && s.fullWidth, s[color])} onClick={onClick}>
       {children}
     </button>
   );
