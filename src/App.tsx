320a1aa3fc50f0a152321040bc38c6fb8792096c
kaneru <kaneru@fastmail.com>
2020-11-10 17:32:44 +0900

    feat: add NotFound page to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { useRoutes } from 'hookrouter';
 
 import routes from './routes';
+import NotFoundPage from './pages/NotFound';
 
 const App: React.FC = () => {
   const match = useRoutes(routes);
 
-  return match;
+  return match || <NotFoundPage />;
 };
 
 export default App;
