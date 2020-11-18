302a2f0e65c8891b6ee193b190593d866ded5e4b
kaneru <kaneru@fastmail.com>
2020-11-19 01:09:48 +0900

    refactor: handle query and deps in useData hook

--- src/hook/getData.ts
+++ src/hook/getData.ts
@@ -20,7 +20,7 @@ interface IPokemonData {
   };
 }
 
-const useData = (endpoint: string) => {
+const useData = (endpoint: string, query: object, deps: any[] = []) => {
   const [data, setData] = useState<IPokemonGlobal>();
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
@@ -29,7 +29,7 @@ const useData = (endpoint: string) => {
     const getData = async () => {
       setIsLoading(true);
       try {
-        const result = await req(endpoint);
+        const result = await req(endpoint, query);
         setData(result);
       } catch (e) {
         setIsError(true);
@@ -38,7 +38,7 @@ const useData = (endpoint: string) => {
       }
     };
     getData();
-  }, []);
+  }, deps);
 
   return {
     data,
