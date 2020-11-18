fb6e90545553d7df6031727292d0ba67f887da34
kaneru <kaneru@fastmail.com>
2020-11-19 01:09:09 +0900

    feat: add query param to utils

--- src/utils/getUrlWithParamsConfig.ts
+++ src/utils/getUrlWithParamsConfig.ts
@@ -1,11 +1,14 @@
 import config from '../config';
 
-function getUrlWithParamsConfig(endpointConfig: string) {
+function getUrlWithParamsConfig(endpointConfig: string, query: object) {
   const url = {
     ...config.client.server,
     ...config.client.endpoint[endpointConfig as keyof typeof config.client.endpoint].uri,
     // endpointConfig as keyof typeof config.client.endpoint
     // endpointConfig может быть одним из ключей в endpoint
+    query: {
+      ...query,
+    },
   };
   return url;
 }
