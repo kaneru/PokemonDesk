cad4a132f7037b83bc5805ee03fad894786b9438
kaneru <kaneru@fastmail.com>
2020-11-19 00:20:43 +0900

    refactor: add getData hook for fetching pokemons

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,52 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import s from './Pokedex.module.scss';
 
 import Layout from '../../components/Layout';
 import PokemonCard from '../../components/PokemonCard';
 import Heading from '../../components/Heading';
-
-interface Pokemon {
-  name: string;
-  id: number;
-  img: string;
-  types: string[];
-  stats: {
-    attack: number;
-    defense: number;
-  };
-}
-
-const usePokemons = () => {
-  const [data, setData] = useState<{ total: number; pokemons: Pokemon[] }>({ total: 0, pokemons: [] });
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    const getPokemons = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch('http://zar.hosthot.ru/api/v1/pokemons');
-        const result = await response.json();
-        setData(result);
-      } catch (e) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    getPokemons();
-  }, []);
-
-  return {
-    data,
-    isLoading,
-    isError,
-  };
-};
+import useData from '../../hook/getData';
 
 const PokedexPage = () => {
-  const { data, isLoading, isError } = usePokemons();
+  const { data, isLoading, isError } = useData('getPokemons');
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Something is wrong!</div>;
@@ -56,10 +18,10 @@ const PokedexPage = () => {
       <Layout>
         <div className={s.wrapper}>
           <Heading tag="h3">
-            {data.total} <b>Pokemons</b> for you to choose your favorite
+            {data?.total} <b>Pokemons</b> for you to choose your favorite
           </Heading>
           <div className={s.pokemonsGrid}>
-            {data.pokemons.map((pokemon) => (
+            {data?.pokemons.map((pokemon) => (
               <PokemonCard
                 key={pokemon.id}
                 name={pokemon.name}
